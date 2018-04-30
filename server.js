223f4db9242afc91f2cbb5e5ad9f395d07474911
Dan Evron <danevron@gmail.com>
2018-04-30 09:37:53 +0300

    Adds projects view

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
@@ -32,7 +36,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Good to see you'
   });
 });
@@ -40,8 +43,14 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Good to see you'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects page',
+    welcomeMessage: 'Projects will go here'
   });
 });
 
